8f520e74d003aae5f834f37304a34cb3fc851e3b
namecoder1 <bartolomei.private@gmail.com>
2026-08-04 11:29:06 +0200

    fix: image url and method fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'cdn.sanity.io'
+    }]
+  },
 };
 
 export default withNextIntl(nextConfig);
